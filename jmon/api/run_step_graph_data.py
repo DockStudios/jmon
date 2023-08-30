a15f4156096a6808a91a0168eb046f0add5ace06
Matthew John <matthew@dockstudios.co.uk>
2023-08-30 06:16:21 +0100

    chore: Move graph data to use classes to generate data
    Issue #10

--- jmon/api/run_step_graph_data.py
+++ jmon/api/run_step_graph_data.py
@@ -9,6 +9,140 @@ from jmon.run_step_data import RunStepData
 from jmon.artifact_storage import ArtifactStorage
 
 
+class BaseGraphNode:
+
+    @property
+    def id(self):
+        """Return ID"""
+        raise NotImplementedError
+
+    @property
+    def depth(self):
+        """Depth of child nesting"""
+        raise NotImplementedError
+
+    @property
+    def step_tree_itx(self):
+        """Depth of child nesting"""
+        raise NotImplementedError
+
+    @property
+    def x(self):
+        """Return X co-ordinate"""
+        return self.root_step.column_x + ((120 / self.root_step.max_child_depth) * self.depth)
+
+    @property
+    def y(self):
+        """Return Y co-ordinate"""
+        return (self.step_tree_itx * 65) + 110
+
+    def __init__(self, step_data, root_step, step_itx):
+        self.step_data = step_data
+        self.root_step = root_step
+        self.step_itx = step_itx
+        self.children = self.get_child_steps()
+
+    def get_element_data(self):
+        """Return element data"""
+        return {
+            "id": self.id,
+            "type": "end",
+            "text": self.step_data.get("description"),
+            "fill": "#F35C4F",
+            "stroke": "#F35C4F",
+            "fontColor": "#FFF",
+            "x": self.x,
+            "y": self.y
+        }
+
+    def get_child_steps(self):
+        """Get child step objects"""
+        # Update root max children
+        return [
+            ChildStepNode(step_data=child_data, root_step=self.root_step, parent_node=self, step_itx=child_itx)
+            for child_itx, child_data in enumerate(self.step_data.get("children"))
+        ]
+
+    def get_child_step_ids(self):
+        """Return list of child step IDs"""
+        child_ids = [self.id]
+        for child in self.children:
+            child_ids += child.get_child_step_ids()
+        return child_ids
+
+    def get_all_elements(self):
+        """Return all data"""
+        elements = [self.get_element_data()]
+        for child in self.children:
+            elements += child.get_all_elements()
+        return elements
+
+
+class ChildStepNode(BaseGraphNode):
+    
+    @property
+    def depth(self):
+        """Depth of child nesting"""
+        return self.parent_node.depth + 1
+
+    @property
+    def step_tree_itx(self):
+        """Depth of child nesting"""
+        return self.parent_node.step_tree_itx + (self.step_itx + 1)
+
+    @property
+    def id(self):
+        """Return ID"""
+        return f"{self.parent_node.id}.{self.step_itx + 1}"
+
+    def __init__(self, parent_node, root_step, *args, **kwargs):
+        self.parent_node = parent_node
+        self.previous_root_step = root_step.previous_root_step
+        super(ChildStepNode, self).__init__(*args, **kwargs, root_step=root_step)
+        self.root_step.max_child_depth = max(self.root_step.max_child_depth, self.depth)
+
+
+class RootGraphData(BaseGraphNode):
+
+    @property
+    def depth(self):
+        """Depth of child nesting"""
+        return 1
+
+    @property
+    def step_tree_itx(self):
+        return 1
+
+    @property
+    def column_x(self):
+        """Return X cordinate for start of column"""
+        return self.step_itx * 300
+
+    @property
+    def id(self):
+        """Return ID"""
+        return f"s{self.step_itx + 1}"
+
+    def __init__(self, previous_root_step, *args, **kwargs):
+        self.max_child_depth = 1
+        self.previous_root_step = previous_root_step
+        self.parent_node = None
+        super(RootGraphData, self).__init__(*args, **kwargs, root_step=self)
+
+    def get_column_data(self):
+        """Return column data"""
+        return {
+            "id": self.step_itx + 1,
+            "type": "$sgroup",
+            "groupChildren": self.get_child_step_ids(),
+            "style": {
+                "fill": "rgba(60, 201, 122, 0.05)"
+            },
+            "x": self.step_itx * 298.75,
+            "y": 80
+        }
+
+
 @FlaskApp.app.route('/api/v1/checks/<check_name>/environments/<environment_name>/runs/<timestamp>/step-graph-data', methods=["GET"])
 def get_run_step_graph_data(check_name, environment_name, timestamp):
     """Obtain run details"""
@@ -44,78 +178,20 @@ def get_run_step_graph_data(check_name, environment_name, timestamp):
     if not step_data:
         return {}, 404
 
-    print(step_data)
     root_steps = step_data.get("children", [])
 
-    graph_root_steps = []
-    graph_steps = []
-    root_x_depths = []
 
-    def process_root_step_children(root_step_children, root_step_itx, parent_name, x, y):
-        nonlocal graph_steps
-        nonlocal root_x_depths
-        child_step_ids = []
-        root_x_depths[root_step_itx] = max(root_x_depths[root_step_itx], x)
-        
-        for child_itx, child in enumerate(root_step_children):
-            id_ = f"{parent_name}.{child_itx + 1}"
-            child_step_ids.append(id_)
-            graph_steps.append({
-                "id": id_,
-                "type": "end",
-                "text": child.get("description"),
-                "fill": "#F35C4F",
-                "stroke": "#F35C4F",
-                "fontColor": "#FFF",
-                "x": x,
-                "y": y
-            })
-            child_step_ids += process_root_step_children(child.get("children"), root_step_itx=root_step_itx, parent_name=id_, x=x+65, y=y+20)
-        return child_step_ids
+    column_data = []
+    graph_elements = []
+    previous_root_step = None
+    for root_step_itx, root_step_data in enumerate(root_steps):
+        root_step_obj = RootGraphData(step_data=root_step_data, step_itx=root_step_itx, previous_root_step=previous_root_step)
 
-    for root_step_itx, root_step in enumerate(root_steps):
-        # Copy root depth from previous root step
-        root_step_x = 0
-        if root_step_itx > 0:
-            root_step_x = root_x_depths[root_step_itx - 1]
-        previous_step_x = 0
-        if root_step_itx > 1:
-            previous_step_x = root_x_depths[root_step_itx - 2]
+        column_data.append(root_step_obj.get_column_data())
+        graph_elements += root_step_obj.get_all_elements()
 
-        # Ensure the start of this is at least 300 since previous, if not the first
-        # column
-        if root_step_itx > 0:
-            root_step_x = max(root_step_x, previous_step_x + 300)
-        root_x_depths.append(root_step_x)
+        previous_root_step = root_step_obj
 
-        # Add element for root step
-        graph_steps.append({
-            "id": f"s{root_step_itx + 1}",
-            "type": "end",
-            "text": root_step.get("description"),
-            "fill": "#F35C4F",
-            "stroke": "#F35C4F",
-            "fontColor": "#FFF",
-            "x": root_step_x,
-            "y": 80
-        })
-        # Add column data
-        graph_root_steps.append({
-            "id": root_step_itx + 1,
-            "type": "$sgroup",
-            "groupChildren": process_root_step_children(
-                root_step.get("children"),
-                root_step_itx,
-                parent_name=f"s{root_step_itx + 1}",
-                x=(root_step_x + 85),
-                y=50
-            ),
-            "style": {
-                "fill": "rgba(60, 201, 122, 0.05)"
-            },
-            "x": root_step_x + 20,
-            "y": 30
-        })
 
     return [
         {
@@ -143,6 +219,6 @@ def get_run_step_graph_data(check_name, environment_name, timestamp):
                 ]
             }
         },
-        *graph_root_steps,
-        *graph_steps
+        *column_data,
+        *graph_elements
     ]
