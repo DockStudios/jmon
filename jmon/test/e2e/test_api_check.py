488705edbf7b1dc5f37ecab0e3994b2b16aab023
Matthew John <matthew@dockstudios.co.uk>
2023-09-01 07:29:23 +0100

    test: Add initial structure for running end-to-end tests using docker-compose
    Issue #101

---
+++ jmon/test/e2e/test_api_check.py
@@ -0,0 +1,13 @@
+
+import os
+
+import requests
+
+
+class TestApiCheck:
+
+
+    def test_register_check(self):
+        """Register check with API"""
+        pass
+
---
+++ test/e2e/run.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set +e
+set -x
+
+docker_compose_project=jmon-e2e
+
+docker-compose --env-file=./.env -p $docker_compose_project -f ./test/e2e/docker-compose.yml up --build --exit-code-from testrunner
+rc=$?
+
+# Get logs from testrunner
+set +x
+echo "----------------- TEST LOGS -----------------------"
+docker-compose --env-file=./.env -p $docker_compose_project -f ./test/e2e/docker-compose.yml logs testrunner
+echo "---------------------------------------------------"
+set -x
+
+docker-compose --env-file=./.env -p $docker_compose_project -f ./test/e2e/docker-compose.yml down -v
+
+exit $rc
