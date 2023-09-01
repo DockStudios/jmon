52d94154570019e76a5b34287baffc5da2b08b46
Matthew John <matthew@dockstudios.co.uk>
2023-09-01 07:43:45 +0100

    test: Bring up databases and DB migrations before running tests
    Issue #101

--- test/e2e/run.sh
+++ test/e2e/run.sh
@@ -5,6 +5,15 @@ set -x
 
 docker_compose_project=jmon-e2e
 
+# Bring up databases
+docker-compose --env-file=./.env -p $docker_compose_project -f ./test/e2e/docker-compose.yml up --quiet-pull -d database broker redis minio
+# Wait
+sleep 30
+
+# Run migration apps and wait for completion
+docker-compose --env-file=./.env -p $docker_compose_project -f ./test/e2e/docker-compose.yml up --quiet-pull createbucket dbupgrade
+
+# Run tests
 docker-compose --env-file=./.env -p $docker_compose_project -f ./test/e2e/docker-compose.yml up --quiet-pull --build --exit-code-from testrunner
 rc=$?
 
