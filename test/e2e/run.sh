d0081aaadaca62007d391316b45fce27cb106cc4
Matthew John <matthew@dockstudios.co.uk>
2024-02-21 07:47:18 +0000

    test: Update tests to use both docker-compose files, removing extended services.
    This should fix the issues seen in CI pipeline (not observed locally):
        + docker-compose --env-file=./.env -p jmon-e2e -f ./test/e2e/docker-compose.yml up --quiet-pull -d database broker redis minio
        service "flower" can't be used with `extends` as it declare `links`
    
    Issue #115

--- test/e2e/run.sh
+++ test/e2e/run.sh
@@ -6,24 +6,24 @@ set -x
 docker_compose_project=jmon-e2e
 
 # Bring up databases
-docker-compose --env-file=./.env -p $docker_compose_project -f ./test/e2e/docker-compose.yml up --quiet-pull -d database broker redis minio
+docker-compose --env-file=./.env -p $docker_compose_project -f ./docker-compose.yml -f ./test/e2e/docker-compose.yml up --quiet-pull -d database broker redis minio
 # Wait
 sleep 30
 
 # Run migration apps and wait for completion
-docker-compose --env-file=./.env -p $docker_compose_project -f ./test/e2e/docker-compose.yml up --quiet-pull createbucket dbupgrade
+docker-compose --env-file=./.env -p $docker_compose_project -f ./docker-compose.yml -f ./test/e2e/docker-compose.yml up --quiet-pull createbucket dbupgrade
 
 # Run tests
-docker-compose --env-file=./.env -p $docker_compose_project -f ./test/e2e/docker-compose.yml up --quiet-pull --build --exit-code-from testrunner server testrunner scheduler agent flower
+docker-compose --env-file=./.env -p $docker_compose_project -f ./docker-compose.yml -f ./test/e2e/docker-compose.yml up --quiet-pull --build --exit-code-from testrunner server testrunner scheduler agent flower
 rc=$?
 
 # Get logs from testrunner
 set +x
 echo "----------------- TEST LOGS -----------------------"
-docker-compose --env-file=./.env -p $docker_compose_project -f ./test/e2e/docker-compose.yml logs testrunner
+docker-compose --env-file=./.env -p $docker_compose_project -f ./docker-compose.yml -f ./test/e2e/docker-compose.yml logs testrunner
 echo "---------------------------------------------------"
 set -x
 
-docker-compose --env-file=./.env -p $docker_compose_project -f ./test/e2e/docker-compose.yml down -v >/dev/null
+docker-compose --env-file=./.env -p $docker_compose_project -f ./docker-compose.yml -f ./test/e2e/docker-compose.yml down -v >/dev/null
 
 exit $rc
