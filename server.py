397c4d52cfec9f0412949bc04a783ee8d076b76e
Matthew John <matthew@dockstudios.co.uk>
2024-03-02 06:48:55 +0000

    feat: Add Sentry integration
    Move server-specific packages to server-specific requirements.txt
    Add sentry packages.
    Add configurations for sentry DSN and environment.
    Setup sentry for celary worker and server.
    
    Issue #119

--- server.py
+++ server.py
@@ -1,9 +1,26 @@
 #!python
 
+import sentry_sdk
+from sentry_sdk.integrations.flask import FlaskIntegration
 from flask_cors import CORS
 
 from jmon.api import FlaskApp
+import jmon.config
+
 
 flask_app = FlaskApp()
+
+if jmon.config.Config.get().SENTRY_DSN:
+    sentry_sdk.init(
+        dsn=jmon.config.Config.get().SENTRY_DSN,
+        enable_tracing=True,
+        integrations = [
+            FlaskIntegration(
+                transaction_style="url",
+            ),
+        ],
+        environment=jmon.config.Config.get().SENTRY_ENVIRONMENT,
+    )
+
 CORS(flask_app.app)
 flask_app.app.run(host='0.0.0.0', threaded=True)
