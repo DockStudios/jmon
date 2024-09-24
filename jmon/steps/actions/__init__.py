d43b697efbbe4c23a2fef574ffc45189a8fff84e
Matt Comben <matt.comben@jobsite.co.uk>
2024-09-24 12:36:53 +0100

    feat: Add support to report performance information
    Issue #132

--- jmon/steps/actions/__init__.py
+++ jmon/steps/actions/__init__.py
@@ -5,3 +5,4 @@ from jmon.steps.actions.type_action import TypeAction
 from jmon.steps.actions.press_action import PressAction
 from jmon.steps.actions.screenshot_action import ScreenshotAction
 from jmon.steps.actions.wait_action import WaitAction
+from jmon.steps.actions.report_performance_action import ReportPerformanceAction
